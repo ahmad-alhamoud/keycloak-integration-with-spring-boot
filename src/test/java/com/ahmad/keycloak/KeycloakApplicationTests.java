0d43302a0801796ded86fcbeddf83aeab6391bbe
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-03-17 13:43:51 +0200

    add docker-compose.yml for keycloak

--- src/test/java/com/ahmad/keycloak/KeycloakApplicationTests.java
+++ src/test/java/com/ahmad/keycloak/KeycloakApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KeycloakApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
